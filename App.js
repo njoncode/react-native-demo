6e2d9cddadafb40b5727b5e3100b46f13b369af4
Neha Joshi <neha@njoncode.local>
2021-03-25 01:17:27 +0530

    Styling Exercise

--- App.js
+++ App.js
@@ -1,30 +1,75 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, PickerIOSComponent } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello World!</Text>
+
+      <View style={styles.container}>
+
+        <Text style={styles.text}>Here are some boxes of different colours</Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.boxText}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.boxText}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.boxText}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.boxText}>Orange #cb4b16</Text>
+        </View>
+
       </View>
+
     </SafeAreaView>
-  )
+
+  );
 };
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
-  },
   container: {
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-    backgroundColor: 'red',
-    borderColor: 'blue',
-    borderWidth: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
+    paddingHorizontal: 10,
+    paddingVertical: 40,
+    // justifyContent: 'space-around',
   },
+
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  box: {
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  cyan: {
+    backgroundColor: '#2aa198',
+
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+
+  boxText: {
+    color: 'white',
+    padding: 10,
+    alignItems: 'center'
+  }
 });
+
 
 export default App;
