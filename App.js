79ef8bb951d9135c8ca95b31d7a612e5b45adaeb
Neha Joshi <neha@njoncode.local>
2021-03-25 14:11:04 +0530

    Lists

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+
+const colors = [
+  { colorName: "Grey", hexCode: "#808080" },
+  { colorName: "Blue", hexCode: "#0000FF" },
+  { colorName: "Aqua", hexCode: "#00ffff" },
+  { colorName: "Teal", hexCode: "#008080" },
+  { colorName: "Orange", hexCode: "#FFA500" },
+  { colorName: "Magenta", hexCode: "#FF00FF" },
+  { colorName: "Green", hexCode: "#008000" },
+  { colorName: "Olive", hexCode: "#808000" },
+  { colorName: "DarkBlue", hexCode: "#0000A0" },
+  { colorName: "Lime", hexCode: "#00FF00" },
+  { colorName: "Maroon", hexCode: "#800000" },
+  { colorName: "Navy", hexCode: "#000080" },
+  { colorName: "Red", hexCode: "#FF0000" },
+  { colorName: "Aquamarine", hexCode: "#7fffd4" },
+]
 
 const App = () => {
   return (
     <SafeAreaView>
 
-      <View style={styles.container}>
-
-        <Text style={styles.text}>Here are some boxes of different colours</Text>
-        <ColorBox colorName="Cyan" hexCode="#2aa198" />
-        <ColorBox colorName="Blue" hexCode="#268bd2" />
-        <ColorBox colorName="Magenta" hexCode="#d33682" />
-        <ColorBox colorName="Orange" hexCode="#cb4b16" />
-
-      </View>
+      <FlatList
+        style={styles.container}
+        data={colors}
+        keyExtractor={item => item.colorName}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+        ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+      />
 
     </SafeAreaView>
 
